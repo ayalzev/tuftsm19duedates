5bb052a07ac80f3c4ad7e73bf205384a2a425f79
ayalzev <ayalzev@gmail.com>
2015-10-02 18:28:00 -0400

    updated lots of stuff

--- js/dates.js
+++ js/dates.js
@@ -2,9 +2,15 @@ var listOfAssignments = {
 	"assignments": [
 	{
 		"id":1,
-		"name":"EpiBio Homework Quiz 2",
+		"name":"Epi/Bio Homework Quiz 2",
 		"course":"EpiBio",
 		"due":new Date(2015,9,5,23,59,00,00)
+	},			
+	{
+		"id":8,
+		"name":"Microbiology Small Group",
+		"course":"Microbiology",
+		"due":new Date(2015,9,7,23,59,00,00)
 	},		
 	{
 		"id":5,
@@ -13,29 +19,54 @@ var listOfAssignments = {
 		"due":new Date(2015,9,14,23,59,00,00)
 	},	
 	{
+		"id":9,
+		"name":"Microbiology Small Group",
+		"course":"Microbiology",
+		"due":new Date(2015,9,14,23,59,00,00)
+	},	
+	{
 		"id":2,
-		"name":"EpiBio Homework Quiz 3",
+		"name":"Epi/Bio Homework Quiz 3",
 		"course":"EpiBio",
 		"due":new Date(2015,9,26,23,00,00,00)
 	},
 	{
 		"id":3,
-		"name":"EpiBio Homework Quiz 4",
+		"name":"Epi/Bio Homework Quiz 4",
 		"course":"EpiBio",
 		"due":new Date(2015,10,2,23,59,00,00)
 	},	
 	{
 		"id":4,
-		"name":"EpiBio Paper Critique",
+		"name":"Epi/Bio Paper Critique",
 		"course":"EpiBio",
 		"due":new Date(2015,10,2,23,59,00,00)
 	},	
 	{
-		"id":4,
-		"name":"Ethics Quiz and Case Analysis 2",
+		"id":10,
+		"name":"Microbiology Small Group",
+		"course":"Microbiology",
+		"due":new Date(2015,10,4,23,59,00,00)
+	},
+	{
+		"id":11,
+		"name":"Microbiology Small Group",
+		"course":"Microbiology",
+		"due":new Date(2015,10,6,23,59,00,00)
+	},
+	{
+		"id":6,
+		"name":"SAT Modules",
+		"course":"SAT",
+		"due":new Date(2016,0,1,23,59,00,00)
+	},
+	{
+		"id":7,
+		"name":"Ethics Quiz & Case Analysis 2",
 		"course":"Ethics",
-		"due":new Date(2015,10,2,23,59,00,00)
-	}
+		"due":new Date(2016,2,9,23,59,00,00)
+	},	
+	
 ]
 };
 
@@ -44,11 +75,12 @@ $(document).ready(function(){
 	for(i = 0; i < listOfAssignments.assignments.length; i++){
 		//var dueDay = listOfAssignments.assignments[i].due.getDate();
 		//var dueMonth = listOfAssignments.assignments[i].due.getMonth();
-		var dueDate = listOfAssignments.assignments[i].due.toDateString().split(' 2015')[0];
+		var dueDate = listOfAssignments.assignments[i].due.toDateString().split(' 201')[0];
 		//var dueTime = listOfAssignments.assignments[i].due.toTimeString().split(' ')[0];
 		var desc = listOfAssignments.assignments[i].name;
-		$("#upcoming").append('<li><span class="assDesc">' + desc + '</span> due on <span class="time">' + dueDate + '</li>');
-		
+		//$("#upcoming").append('<li><span class="time">' + dueDate + '</span> ~ <span class="assDesc">' + desc + '</span></li>');
+		$("#upcoming-date").append('<p>' + dueDate + '</p>');
+		$("#upcoming-desc").append('<p>' + desc + '</p>');
 	}
 
 });
